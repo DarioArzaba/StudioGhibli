4bcca900483a5e0ad113a70c50fe364ca063b092
Dario Arzaba <dario.arzaba@gmail.com>
2024-03-01 17:54:08 -0500

    fix index import so that it finds the src dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import {Provider} from 'react-redux';
 import store from './app/store/store';
 
 const App = (): React.JSX.Element => (
