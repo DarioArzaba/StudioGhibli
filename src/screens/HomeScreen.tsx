a50cbe45cefee75ea610efc510d88c1f5c6e5653
Dario Arzaba <dario.arzaba@gmail.com>
2024-03-03 00:52:39 -0500

    added blur effect and top bar stays when loading films

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,55 +1,114 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   ImageBackground,
   SafeAreaView,
   View,
+  StyleSheet,
+  Dimensions,
 } from 'react-native';
-import {StyleSheet} from 'react-native';
-import HomeScreenHeader from '../components/HomeScreenHeader';
-import FilmList from '../components/FilmList';
 import {useSelector} from 'react-redux';
 import {selectFilms, selectIsLoading} from '../app/selectors/filmsSelector';
+import HomeScreenHeader from '../components/HomeScreenHeader';
 import HomeScreenFooter from '../components/HomeScreenFooter';
+import FilmList from '../components/FilmList';
+import FilmListHeader from '../components/FilmListHeader';
 
 const HomeScreen = (): React.JSX.Element => {
+  const [screen, setScreen] = useState(Dimensions.get('window'));
   const films = useSelector(selectFilms);
   const isLoading = useSelector(selectIsLoading);
-  const backgroundImage = {uri: 'https:www.ghibli.jp/gallery/chihiro039.jpg'};
+  const backgroundImageOne = {
+    uri: 'https://www.ghibli.jp/gallery/chihiro039.jpg',
+  };
+  const backgroundImageTwo = {
+    uri: 'https://www.ghibli.jp/gallery/kazetachinu024.jpg',
+  };
+
   const filmsFetched = films && films.length !== 0;
+
+  useEffect(() => {
+    const updateDimensions = () => setScreen(Dimensions.get('window'));
+    const subscription = Dimensions.addEventListener(
+      'change',
+      updateDimensions,
+    );
+
+    return () => subscription.remove(); // Cleanup
+  });
+  const isPortrait = screen.height >= screen.width;
+
   return (
-    <SafeAreaView style={styles.homeScreenSafeAreaView}>
+    <SafeAreaView style={portraitStyles.homeScreenSafeAreaView}>
       <ImageBackground
-        source={backgroundImage}
+        source={filmsFetched ? backgroundImageTwo : backgroundImageOne}
         resizeMode="cover"
-        style={styles.homeScreenBackgroundImage}>
+        blurRadius={5}
+        style={portraitStyles.homeScreenBackgroundImage}>
         {!filmsFetched && (
-          <View>
+          <View
+            style={
+              isPortrait
+                ? portraitStyles.homeScreenHeader
+                : landscapeStyles.homeScreenHeader
+            }>
             <HomeScreenHeader />
           </View>
         )}
         {isLoading && (
-          <ActivityIndicator style={styles.homeScreenLoadingIndicator} />
+          <View style={{width: '100%'}}>
+            <FilmListHeader />
+            <ActivityIndicator
+              color="blue"
+              size={'large'}
+              style={
+                isPortrait
+                  ? portraitStyles.homeScreenLoadingIndicator
+                  : landscapeStyles.homeScreenLoadingIndicator
+              }
+            />
+          </View>
         )}
-        {filmsFetched && !isLoading && <FilmList />}
+        {filmsFetched && !isLoading && (
+          <View>
+            <FilmList />
+          </View>
+        )}
       </ImageBackground>
       {!filmsFetched && <HomeScreenFooter />}
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
+const portraitStyles = StyleSheet.create({
   homeScreenSafeAreaView: {
     flex: 1,
-    justifyContent: 'center',
   },
   homeScreenBackgroundImage: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
   },
   homeScreenLoadingIndicator: {
     marginTop: '50%',
+  },
+  homeScreenHeader: {
+    marginTop: '70%',
+  },
+});
+
+const landscapeStyles = StyleSheet.create({
+  homeScreenSafeAreaView: {
+    flex: 1,
+  },
+  homeScreenBackgroundImage: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  homeScreenLoadingIndicator: {
+    marginTop: '20%',
+  },
+  homeScreenHeader: {
+    marginTop: '10%',
   },
 });
 
