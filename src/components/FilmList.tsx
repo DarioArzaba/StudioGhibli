38ee1e638f486c8f754a734b004c2de6510a3252
Dario Arzaba <dario.arzaba@gmail.com>
2024-03-03 20:49:31 -0500

    add local background assets

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import FilmCard from './FilmCard';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {selectFilms} from '../app/selectors/filmsSelector';
 import {FlatList, View} from 'react-native';
 import Film from '../models/FilmsResponse';
 import FilmListHeader from './FilmListHeader';
+import {getFilms} from '../app/actions/actionCreators';
 
 const FilmList = (): React.JSX.Element => {
   const films = useSelector(selectFilms);
   const filmCard = ({item}: {item: Film}) => (
     <FilmCard key={item.id} film={item} />
   );
+  const dispatch = useDispatch();
+  const loadMoreFilms = () => {
+    dispatch(getFilms());
+  };
 
   return (
     <View>
@@ -19,6 +24,8 @@ const FilmList = (): React.JSX.Element => {
         data={films || []}
         renderItem={filmCard}
         keyExtractor={item => item.id}
+        onEndReached={loadMoreFilms}
+        onEndReachedThreshold={0.8}
       />
     </View>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,17 +9,14 @@ import {
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectFilms, selectIsLoading} from '../app/selectors/filmsSelector';
+import {selectScreenDimensions} from '../app/selectors/uiSelector';
+import {updateOrientationState} from '../app/actions/actionCreators';
 import HomeScreenHeader from '../components/HomeScreenHeader';
 import HomeScreenFooter from '../components/HomeScreenFooter';
 import FilmList from '../components/FilmList';
 import FilmListHeader from '../components/FilmListHeader';
-import {selectScreenDimensions} from '../app/selectors/uiSelector';
-import {updateOrientationState} from '../app/actions/actionCreators';
 
 const HomeScreen = (): React.JSX.Element => {
-  const films = useSelector(selectFilms);
-  const isLoading = useSelector(selectIsLoading);
-  const screenDimensions = useSelector(selectScreenDimensions);
   const dispatch = useDispatch();
   useEffect(() => {
     const updateDimensions = () => dispatch(updateOrientationState());
@@ -29,21 +26,18 @@ const HomeScreen = (): React.JSX.Element => {
     );
     return () => subscription.remove();
   });
-
-  const backgroundImageOne = {
-    uri: 'https://www.ghibli.jp/gallery/chihiro039.jpg',
-  };
-  const backgroundImageTwo = {
-    uri: 'https://www.ghibli.jp/gallery/kazetachinu024.jpg',
-  };
-
+  const films = useSelector(selectFilms);
   const filmsFetched = films && films.length !== 0;
+  const isLoading = useSelector(selectIsLoading);
+  const screenDimensions = useSelector(selectScreenDimensions);
   const isPortrait = screenDimensions.height >= screenDimensions.width;
+  const bgNoFilmsFetched = require('../assets/chihiro039.jpg');
+  const bgFilmsFetched = require('../assets/kazetachinu024.jpg');
 
   return (
     <SafeAreaView style={portraitStyles.homeScreenSafeAreaView}>
       <ImageBackground
-        source={filmsFetched ? backgroundImageTwo : backgroundImageOne}
+        source={filmsFetched ? bgNoFilmsFetched : bgFilmsFetched}
         resizeMode="cover"
         blurRadius={5}
         style={portraitStyles.homeScreenBackgroundImage}>
@@ -71,11 +65,7 @@ const HomeScreen = (): React.JSX.Element => {
             />
           </View>
         )}
-        {filmsFetched && !isLoading && (
-          <View>
-            <FilmList />
-          </View>
-        )}
+        {filmsFetched && !isLoading && <FilmList />}
       </ImageBackground>
       {!filmsFetched && <HomeScreenFooter />}
     </SafeAreaView>
