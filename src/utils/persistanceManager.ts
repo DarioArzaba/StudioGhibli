e49fa28661908c604e0fa898abd1e4cc156971dc
Dario Arzaba <dario.arzaba@gmail.com>
2024-03-08 13:02:25 -0500

    delete redundant persistance manager logs

--- src/utils/persistanceManager.ts
+++ src/utils/persistanceManager.ts
@@ -1,16 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// PersistanceHelper.ts
+// PersistanceManager.ts
+
+// Save a preference locally:
 export const storeData = async (key: string, value: string): Promise<void> => {
   try {
     await AsyncStorage.setItem(key, value);
+    console.log('Data saved!');
   } catch (e) {
     console.error('Error saving data!', e);
   }
 };
 
+// Retrieve a preference from local storage:
 export const readData = async (key: string): Promise<string | null> => {
   try {
     const storedData: string | null = await AsyncStorage.getItem(key);
+    console.log(`Retrieved data with key: ${key}: `, storedData);
     return storedData;
   } catch (e) {
     console.error('Error reading data!', e);
@@ -18,25 +25,30 @@ export const readData = async (key: string): Promise<string | null> => {
   }
 };
 
+// Update a existing value:
 export const updateData = async (
   key: string,
   newValue: string,
 ): Promise<void> => {
   try {
     await AsyncStorage.setItem(key, newValue);
+    console.log(`Data updated to: ${newValue}`);
   } catch (e) {
     console.error('Error updating data!', e);
   }
 };
 
+// Delete a persisted value:
 export const deleteData = async (key: string): Promise<void> => {
   try {
     await AsyncStorage.removeItem(key);
+    console.log(`Data for item: ${key} removed!`);
   } catch (e) {
     console.error('Error removing data!', e);
   }
 };
 
+// Working with JSON:
 export const storeObject = async <T>(
   key: string,
   jsonValue: T,
